999b811f514bcfc1239a2c17cd251658f599dbf9
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-02-05 14:42:59 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 4, "female", "brown", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 8, "male", "black", "spunky", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 2, "female", "black", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 10, "male", "brown", "militant", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 7, "female", "white", "majestic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 18, "male", "black", "grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 9, "female", "marbled", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, NULL, NULL, "black", NULL, 0);
