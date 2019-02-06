7b42f2c35ae7a6730237b87c011a26ee409ef466
halented <hallie.dunn1@gmail.com>
2019-02-06 08:52:20 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES("Mr. Chocolate", 50, "enbie", "phosphorescent", "grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES("Rowdy", 2, "male", "turqiose", "dead", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES("Tabitha", 15, "male", "shiny", "invigorated", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES("Sergeant Brown", 4, "female", "iridescent", "effervescent", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES("Melissa", 9, "enbie", "certain shades of green", "short", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES("Grinch", 147, "ungendered", "passionfruit", "fanatical", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES("Wendy", 0, "unstoppable", "muted", "vibrant", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES(NULL, 10, "female", "vantablack", "turbulent", 0);
