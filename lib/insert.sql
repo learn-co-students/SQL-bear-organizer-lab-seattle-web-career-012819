9bb38a4fc47642879e6ea327933773d7bf9ef045
Jessica Poe <jesspoe00@gmail.com>
2019-02-05 17:43:08 -0800

    Finished SQL queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "female", "brown", "joyful", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "male", "white", "calm", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 9, "female", "black", "cheerful", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", "male", 11, "brown", "anxious", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "female", "blonde", "mature", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 20, "female", "brown", "playful", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "male", "black", "quiet", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 13, "female", "white", "ferocious", 0);
