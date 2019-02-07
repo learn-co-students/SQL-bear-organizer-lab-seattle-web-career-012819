ae818f4d8de8b0d1a36ab58811020a98b1b5e154
github username <github email address>
2019-02-06 16:56:45 -0800

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -7,27 +7,3 @@ CREATE TABLE bears (
   temperament TEXT,
   alive BOOLEAN  
 );
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'happy', 1);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Rowdy', 3, 'male', 'black', 'rambunctious', 1);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Tabitha', 11, 'female', 'brown', 'calm', 1);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Sergeant Brown', 18, 'male', 'black', 'angry', 1);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Melissa', 15, 'female', 'white', 'happy', 1);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Grinch', 45, 'male', 'brown', 'greedy', 1);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Wendy', 25, 'female', 'brown', 'moody', 1);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES (NULL, 19, 'male', 'brown', 'furious', 1);
---
+++ lib/insert.sql
@@ -0,0 +1,56 @@
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+-- VALUES (1, 'Mr. Chocolate', 8, 'male', 'brown', 'happy', 1);
+
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+-- VALUES (2, 'Rowdy', 3, 'male', 'black', 'goofy', 1);
+
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+-- VALUES (3, 'Tabitha', 11, 'female', 'brown', 'calm', 1);
+
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+-- VALUES (4, 'Sergeant Brown', 18, 'male', 'black', 'angry', 1);
+
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+-- VALUES (5, 'Melissa', 15, 'female', 'white', 'happy', 1);
+
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+-- VALUES (6, 'Grinch', 45, 'male', 'brown', 'greedy', 1);
+
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+-- VALUES (7, 'Wendy', 25, 'female', 'brown', 'goofy', 1);
+
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+-- VALUES (8, NULL, 19, 'male', 'brown', 'furious', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 8, 'male', 'brown', 'happy', 1);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive)
+-- VALUES ('Rowdy', 3, 'male', 'black', 'goofy', 1);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive)
+-- VALUES ('Tabitha', 6, 'female', 'brown', 'calm', 1);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive)
+-- VALUES ('Sergeant Brown', 18, 'male', 'black', 'angry', 1);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive)
+-- VALUES ('Melissa', 13, 'female', 'white', 'happy', 1);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive)
+-- VALUES ('Grinch', 45, 'male', 'brown', 'greedy', 1);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive)
+-- VALUES ('Wendy', 6, 'female', 'brown', 'goofy', 1);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive)
+-- VALUES (NULL, 19, 'male', 'brown', 'furious', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES  ('Rowdy', 3, 'M', 'black', 'goofy', 1),
+        ('Tabitha', 6, 'F', 'brown', 'calm', 1),
+        ('Sergeant Brown', 18, 'M', 'black', 'angry', 1),
+        ('Melissa', 13, 'F', 'white', 'happy', 1),
+        ('Grinch', 45, 'M', 'brown', 'greedy', 1),
+        ('Wendy', 6, 'F', 'brown', 'goofy', 1),
+        (NULL, 19, 'M', 'brown', 'furious', 1);
